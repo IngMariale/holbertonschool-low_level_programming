84f52f61df4718306176b8e990f5ea793f093635
IngMariale <4725@holbertonsschool.com>
2022-07-23 19:01:05 -0700

    creat 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - print with write
+ * Return: 1
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+return (1);
+}
