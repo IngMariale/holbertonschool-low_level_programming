438438d52f3954d9629b293d98fb4a4e800ca354
IngMariale <4725@holbertonsschool.com>
2022-07-25 22:48:32 -0700

    change spaces of the 100-first program

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,21 +1,13 @@
 #include "lists.h"
 
 /**
-
  * run_before_main - Write a function that prints
-
  * before the main function is executed
-
  * Return: Always 0.
-
  */
 
 void run_before_main(void)
-  
 {
-  
-  printf("You're beat! and yet, you must allow,\n");
-  
-  printf("I bore my house upon my back!\n");
-  
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
