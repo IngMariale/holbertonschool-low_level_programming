77605e3bd85724c1b92f0ed3f2909f4390b7ff0d
IngMariale <4725@holbertonsschool.com>
2022-07-25 22:18:18 -0700

    update 0-print_list

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+
+ * run_before_main - Write a function that prints
+
+ * before the main function is executed
+
+ * Return: Always 0.
+
+ */
+
+void run_before_main(void)
+  
+{
+  
+  printf("You're beat! and yet, you must allow,\n");
+  
+  printf("I bore my house upon my back!\n");
+  
+}
