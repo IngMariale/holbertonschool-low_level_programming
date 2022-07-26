5598d358124cf6fd04f74df513e82f3388f1b49e
IngMariale <4725@holbertonsschool.com>
2022-07-25 22:32:46 -0700

    remove 2 spaces

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,10 +15,10 @@ while (h != NULL)
 {
 if (h->str == NULL)
 {
-printf("[0] nil \n");
+printf("[0] nil\n");
 }
 else
-printf("[%d] %s \n", h->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 i++;
 }
