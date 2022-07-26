9925e938ed0f419148e894dd78d08676c9639694
IngMariale <4725@holbertonsschool.com>
2022-07-26 09:18:05 -0700

    add new spaces on for and return

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,10 +10,11 @@ int main(void)
 int i = 0;
 char content[] = "_putchar";
 
-for(; content[i] != '\0'; i++)
+for (; content[i] != '\0'; i++)
+{
 _putchar(content[i]);
-
+}
 _putchar('\n');
 
-return(0);
+return (0);
 }
