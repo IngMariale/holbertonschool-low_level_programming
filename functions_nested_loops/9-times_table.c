786bf2738fbb9cdf4101f8be2770b5c84f6b8f34
IngMariale <4725@holbertonsschool.com>
2022-07-27 09:11:13 -0700

    create 9-times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)  
+{
+
+long int i = 0, j = 0, k = 0, l = 0;
+
+for (j = 0; j <= 9; j++)    
+{      
+for (i = 0; i <= 9; i++)	
+{	  
+k = i * j;	  
+if (k > 9)	    
+{
+l = k % 10;	      
+k = k / 10;	      
+_putchar(' ');
+_putchar('0' + k);
+_putchar('0' + l);
+if (i != 9)
+{
+_putchar(',');
+}
+}	  
+else	    
+{	      
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + k);
+if(i != 9)
+{
+_putchar(',');
+}
+}	  
+}      
+_putchar('\n');      
+}
+}  
