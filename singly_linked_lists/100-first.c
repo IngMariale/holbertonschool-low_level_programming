02f30f6f7f062fd7d79273a07358efe7af68eed1
IngMariale <4725@holbertonsschool.com>
2022-07-29 20:36:54 -0700

    change name of the prototype

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * run_before_main - Write a function that prints
+ * first - Write a function that prints
  * before the main function is executed
  * Return: Always 0.
  */
 
-void run_before_main(void)
+void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
