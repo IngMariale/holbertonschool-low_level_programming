e2b99590f2ed45ccb2da1cf8629c850932de7b9b
IngMariale <4725@holbertonsschool.com>
2022-07-29 22:08:38 -0700

    create 5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a list
+* @head: header from list
+*Return: -
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *f;
+
+if (head == NULL)
+{
+return;
+}
+while (*head)
+{
+f = (*head)->next;
+free(*head);
+*head = f;
+}
+  
+*head = NULL;
+}
