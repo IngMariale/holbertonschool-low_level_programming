6b050fb49e6430dfb161adb8f5433622156796d0
IngMariale <4725@holbertonsschool.com>
2022-07-29 20:29:39 -0700

    create 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list
+* @head: is the beginning of the list
+*/
+
+void free_list(list_t *head)
+{
+if (head == NULL)
+{
+return;
+}
+free_list(head->next);
+free(head->str);
+free(head);
+}
