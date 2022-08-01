197fbaced196ff0e0bb658acc9bd9280ac26f763
IngMariale <4725@holbertonsschool.com>
2022-07-31 21:49:18 -0700

    add an if for the space requiarment

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,26 +15,31 @@ for (i = 1; i <= 100; i++)
 
 if (i % 3 != 0 && i % 5 != 0)
 {
-printf("%d ", i);
+printf("%d", i);
 }
 
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else
 {
 
 if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 
 if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 
+}
+
+if (i != 100)
+{
+printf(" ");
 }
 
 }
