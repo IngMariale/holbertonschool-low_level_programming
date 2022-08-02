95257b031cbb3f4c5cf6b8916fa5f14695d19f85
IngMariale <4725@holbertonsschool.com>
2022-08-01 18:04:33 -0700

    create 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - pointer to an int as updates the value it points to to 98
+* @n: pointer to the int value
+*/
+
+void reset_to_98(int *n)
+{
+int *j = n;
+
+*j = 98;
+}
