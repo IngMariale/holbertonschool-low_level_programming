5b2d331dc73a78dea877a0cb3183456360ac369e
IngMariale <4725@holbertonsschool.com>
2022-08-02 15:02:20 -0700

    cretae 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* *_strcpy - copies string pointed to by src to the buffer pointed to by dest
+* @dest: pointer destination
+* @src: pointer copie from
+* Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0, len = 0;
+
+while (src[len])
+{
+len++;
+}
+
+for (; i < len; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
