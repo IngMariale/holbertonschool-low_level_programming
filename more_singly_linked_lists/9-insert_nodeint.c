75c6198bfa3b170771daaacafc3b3ca276d26e13
IngMariale <4725@holbertonsschool.com>
2022-08-03 12:34:13 -0700

    change logit in the while, add int i

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,22 +10,22 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-
+unsigned int i = 0;
 listint_t *temp = *head;
 listint_t *new_node =  malloc(sizeof(listint_t));
 
-if (new_node == NULL || head == NULL )
+if (new_node == NULL || head == NULL)
 {
 return (NULL);
 } /*end if*/
 
-new_node->n = n;
-new_node->next = NULL;
+new_node->n = n; /*data of the node provided assigned to new_node*/
+new_node->next = NULL; /*assigned the address NULL to new_node*/
 
-while (idx != 1)
+while (i < (idx - 1))
 {
-temp = temp->next;
-idx--;
+temp = temp->next; /*move temp to the next position*/
+i++;
 } /*end while*/
 
 new_node->next = temp->next;
