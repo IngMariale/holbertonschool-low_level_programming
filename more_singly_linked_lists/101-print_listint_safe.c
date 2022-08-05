01241ea9ea34ba2234552d0b3b5b3e25864c6b1e
IngMariale <4725@holbertonsschool.com>
2022-08-04 23:24:27 -0700

    create 101-print_listint_safe.c not working

---
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - prints a linked list
+* @head: Pointer from structure
+* Return: number of nodes
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+  
+size_t number = 0;
+  
+ 
+  while (head != NULL)
+    {
+      
+      /* _putchar('0' + head->n);*/
+	 /*_putchar('\n');*/
+      
+      head = head->next;
+      
+      number++;
+      
+    }
+  
+  return (number);
+  
+}
