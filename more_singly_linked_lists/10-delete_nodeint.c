46ece7765ad7e1f2d05d69268ad73819c5060d05
IngMariale <4725@holbertonsschool.com>
2022-08-04 21:53:35 -0700

    correct logic correct output, nned to chnage betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -11,58 +11,63 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *temp = *head, *prev = *head;
- 
-if (head == NULL) /*don't exist head 1*/
+
+if (head == NULL) /*don't exist head */
 {
 return (-1);
-} /*end if-don't exist head 1*/
+} /*end if-don't exist head */
 
- for (i = 0; i <= index; i++) /*index in the middle*/
-   {
+for (i = 0; i <= index; i++) /*index in the middle*/
+{
 
-     if (i == 0 && index == 0)
-       {
-	 *head = temp->next;
+if (i == 0 && index == 0)
+{
 
-	 if (head == NULL)
-	   {
-	     return(-1);
-	   }
-	 
-	 return(1);
-	 free(temp);
-       }
-     else
-       {
+if(*head == NULL)
+{
+return (-1);
+}
 
-	 if(i == index - 1 && temp)
-       {
-	 prev = temp;
-	 if (prev == NULL)
-	   {
-	     return (-1);
-	   }
-	 temp = temp->next;
-	 prev->next = temp->next;
-	 free(temp);
-	 return (1);
-       } /*end if*/
-        else
-       {
-	 prev = temp;
-	 
-	 if(prev == NULL)
-	   {
-	     return(-1);
-	   } /*end if */
-	 
-	 temp = temp->next;
-       } /*end else*/
-	 
-       } /*end else*/
-     
-   } /*end for-index in the middle*/
+*head = temp->next;
 
+if (head == NULL)
+{
+return(-1);
+}
+
+return(1);
+free(temp);
+}
+else
+{
+
+if(i == index - 1 && temp)
+{
+prev = temp;
+if (prev == NULL)
+{
+return (-1);
+}
+temp = temp->next;
+prev->next = temp->next;
+free(temp);
+return (1);
+} /*end if*/
+else
+{
+prev = temp;
+
+if(prev == NULL)
+{
+return(-1);
+} /*end if */
+
+temp = temp->next;
+} /*end else*/
+
+} /*end else*/
+
+} /*end for-index in the middle*/
 
 return (1);
 
