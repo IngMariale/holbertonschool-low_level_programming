0d6057328afd8bbc6f120a4aa970e174ed745098
IngMariale <4725@holbertonsschool.com>
2022-08-04 23:27:52 -0700

    correct betty

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,23 @@
 #include "lists.h"
 
 /**
-* print_listint_safe - prints a linked list
+* print_listint_safe - prints a linked list-NOT WORKING
 * @head: Pointer from structure
 * Return: number of nodes
 */
 
 size_t print_listint_safe(const listint_t *head)
 {
-  
+
 size_t number = 0;
-  
- 
-  while (head != NULL)
-    {
-      
-      /* _putchar('0' + head->n);*/
-	 /*_putchar('\n');*/
-      
-      head = head->next;
-      
-      number++;
-      
-    }
-  
-  return (number);
-  
+
+while (head != NULL)
+{
+/* _putchar('0' + head->n);*/
+/*_putchar('\n');*/
+
+head = head->next;
+number++;
+}
+return (number);
 }
