eb15eb03ee31bdb24168551912921af08b4c96e9
IngMariale <4725@holbertonsschool.com>
2022-08-04 22:54:19 -0700

    correct betty

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -11,16 +11,16 @@
 * @next: points to the next node
 *
 * Description: singly linked list node structure
-* 
+*
 */
 
 typedef struct listint_s
 {
-int n; 
+int n;
 struct listint_s *next;
 } listint_t;
 
-int calculations();
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
