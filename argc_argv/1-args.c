2401f0e6bf46d8276e805364ce5c16813cfe9ee1
IngMariale <4725@holbertonsschool.com>
2022-08-23 13:14:41 -0700

    create 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed into it
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int i = argc - 1;
+
+printf("%d\n", i);
+
+return (0);
+}
