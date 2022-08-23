87c4572d55d0244f7044817228131dab7d2c5af0
IngMariale <4725@holbertonsschool.com>
2022-08-22 20:48:01 -0700

    create 5-square_recursion, not finsh

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number
+* Return: natural square root of a number
+*/
+
+int _sqrt_recursion(int n);
+{
+  int p;
+ 
+  if (n == 0 || n == 1)
+    {      
+      return (n); 
+    }
+  
+  
+   
+
+  
+}  
