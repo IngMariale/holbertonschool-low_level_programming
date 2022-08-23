0e1da93783570b8390a5ab43d3e71fe0be2bf58b
IngMariale <4725@holbertonsschool.com>
2022-08-23 09:36:37 -0700

    complete with help 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,17 +6,44 @@
 * Return: natural square root of a number
 */
 
-int _sqrt_recursion(int n);
+int _sqrt_recursion(int n)
 {
-  int p;
- 
-  if (n == 0 || n == 1)
-    {      
-      return (n); 
-    }
-  
-  
-   
 
-  
-}  
+if (n == 0 || n == 1)
+{
+return (n);
+}
+
+if (n < 0)
+{
+return (-1);
+}
+
+return (root_check(n, 1));
+
+}
+
+/**
+* root_check - finds the square root of a natural number
+* @square: number from were will be find the square
+* @root: integer checked to be the square of the number received (square)
+* Return: the square root or -1 if there no natural square root
+*/
+
+int root_check(int square, int root)
+{
+
+if (square == (root * root))
+{
+return (root);
+}
+else if (root * root > square)
+{
+return (-1);
+}
+else
+{
+return (root_check(square, root + 1));
+}
+
+}
