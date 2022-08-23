447ff4f7e6f03c95e3fc2c45b266c4e8277cf606
IngMariale <4725@holbertonsschool.com>
2022-08-23 15:05:56 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program that adds positive numbers.
+* Print the result, followed by a new line
+* If no number is passed to the program, print 0,
+* followed by a new line
+* If one of the number contains symbols that are
+* not digits, print Error, followed by a new line,
+* and return 1
+* You can assume that numbers and the addition of
+* all the numbers can be stored in an int
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 or 1 if error
+*/
+
+int main(int argc, char *argv[])
+{
+int i = 1, add = 0, temp = 0;
+
+for (; i < argc; i++)
+{
+temp = strtol(argv[i], argv, 10);
+
+if (temp > 0)
+{
+add = add + temp;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", add);
+
+return (0);
+}
